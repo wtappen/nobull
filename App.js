f28978c1767f89aba0c62da96649bacea5089507
kristjanari <kristjant18@ru.is>
2022-02-26 21:23:12 -0500

    first commit, set upp react native app

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Will doesn't know how to download Xcode</Text>
       <StatusBar style="auto" />
     </View>
   );
